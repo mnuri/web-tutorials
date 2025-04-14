7f92bc75bc63594b5e052949c869b7fe66166938
Kuraev Ilya <kuraev.nn@gmail.com>
2025-04-14 17:50:33 +0300

    fix

--- django/fast_track/settings.py
+++ django/fast_track/settings.py
@@ -100,7 +100,7 @@ DATABASES = {
         "PASSWORD": os.getenv("POSTGRES_PASSWORD", "postgres"),
         "HOST": os.getenv("POSTGRES_HOST", "localhost"),
         "PORT": os.getenv("POSTGRES_PORT", "5432"),
-    },
+    }
 }
 
 # Password validation
